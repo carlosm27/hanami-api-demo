9d8036093f4d36276b5f9f318ccdd7f8be3d783c
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-03-28 14:16:55 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,30 +1,66 @@
 
-
-Tasks = [
-    {"id": 1, "title": "Buy almonds"},
-    {"id": 2, "title": "Buy Nuts"}
-]
-
 class App < Hanami::API
+    list_tasks = [
+        {"id": 1, "title": "Buy almonds"},
+        {"id": 2, "title": "Buy Nuts"}
+    ]
     use Hanami::Middleware::BodyParser, :json
+
     get "/" do
       "Hello, world"
     end
 
     get "/tasks" do
-       json Tasks
+       json list_tasks
     end
 
     post "/task" do
         puts params
         
-        Tasks.push(params)
+        list_tasks.push(params)
         json params
     end 
     
     get "/task/:id" do
         id = params[:id]
-        task = Tasks.find{|x| x[:id] == id.to_i}
-        json task
+        task = list_tasks.find{|x| x[:id] == id.to_i}
+
+        if task == nil
+            json ({status: status(404), error: "not_found", message: "Entry #{params[:id]} not found"})
+    
+        else
+            json task
+               
+        end  
+       
+    end
+    
+    put "/task/:id" do
+        id = params[:id]
+        task = list_tasks.find{|x| x[:id] == id.to_i}
+
+        if task == nil
+            json ({status: status(404), error: "not_found", message: "Entry #{params[:id]} not found"})
+    
+        else
+            
+            
+            json list_tasks[list_tasks.rindex(task)] = params
+               
+        end  
+    end  
+    
+    delete "/task/:id" do
+        id = params[:id]
+        task = list_tasks.find{|x| x[:id] == id.to_i}
+
+        if task == nil
+            json ({status: status(404), error: "not_found", message: "Entry #{params[:id]} not found"})
+    
+        else
+            
+            list_tasks.delete_at(list_tasks.rindex(task))
+            json(message: "Task deleted")
+        end
     end    
 end  
